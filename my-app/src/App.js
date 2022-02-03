d060cb39343244c92ada047a119e3deaa9bd4aba
nnk-maker <krishnanarasimha4@gmail.com>
2022-02-03 11:25:38 +0100

    class component
    class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-function App() {
+
+
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={title: "Hello React 2"};
+
+    }
+    render(){
+        return <div>{this.state.title}</div>;
+    }
+}
+
+/* function App() {
 
     return <div>Hello React</div>;
     
-}
+} */
 
 export default App;
