f6e9adc6d97a319b6f3aa030fc08990385017125
nnk-maker <krishnanarasimha4@gmail.com>
2022-02-03 11:12:17 +0100

    function component
    function component

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+//const App = require("./App");
+
+ReactDOM.render(<App/>, document.getElementById("root"));
