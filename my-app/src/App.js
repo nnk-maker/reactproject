c339ee58e00ba0e37d0208c8404ef09cc0d291bb
nnk-maker <krishnanarasimha4@gmail.com>
2022-02-08 09:42:04 +0100

    tailwindcss working
    tailwindcss working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./assets/css/tailwind.css"
+import "./assets/css/style.css"
 
 
 
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  future: {},
-  purge: [],
+  // You are missing this block that defines what files tailwind should scan for usage
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
