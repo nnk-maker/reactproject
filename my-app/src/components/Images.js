6ba99e8300dc7469cdce4f524c65cae3664bf979
nnk-maker <krishnanarasimha4@gmail.com>
2022-02-15 13:54:33 +0100

    functional component useEffect
    functional component useEffect

--- my-app/src/components/Images.js
+++ my-app/src/components/Images.js
@@ -1,6 +1,31 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useEffect, useState } from 'react';
 
-export default class images extends PureComponent {
+
+
+export default function Images() {
+    
+   // const [myInterval, setmyInterval] = useState(null);
+    
+    useEffect( () => {
+         console.log("Images comp mounted");
+          const interval =  setInterval( ()=> {
+            console.log("Hello");
+            },1000 );
+          return () => {
+              //console.log("Images Unmount");
+              clearInterval(interval);
+          }
+    },
+    [] );
+
+
+  return (
+    <img src="https://images.unsplash.com/photo-1643685623589-625d36ffb709?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" />
+  );
+}
+
+
+/* export default class images extends PureComponent {
 
     constructor(props){
         console.log(props);
@@ -33,3 +58,4 @@ export default class images extends PureComponent {
     );
   }
 }
+ */
