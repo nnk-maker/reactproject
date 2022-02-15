d7dcf93411a584fb97293995bddc94bceb7834c6
nnk-maker <krishnanarasimha4@gmail.com>
2022-02-15 16:22:26 +0100

    component inside component
    component inside component

--- my-app/src/components/Images.js
+++ my-app/src/components/Images.js
@@ -4,58 +4,35 @@ import React, { PureComponent, useEffect, useState } from 'react';
 
 export default function Images() {
     
-   // const [myInterval, setmyInterval] = useState(null);
-    
-    useEffect( () => {
-         console.log("Images comp mounted");
-          const interval =  setInterval( ()=> {
-            console.log("Hello");
-            },1000 );
-          return () => {
-              //console.log("Images Unmount");
-              clearInterval(interval);
-          }
-    },
-    [] );
+   const Images = [
+
+    "https://images.unsplash.com/photo-1643685623589-625d36ffb709?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+    "https://images.unsplash.com/photo-1644866679372-11af353e9b0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "https://images.unsplash.com/photo-1644871829528-20cc68a1bf17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    "https://images.unsplash.com/photo-1587870306141-4f19861e6c73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80"
 
 
+   ];
+
+   function ShowImage(){
+           return Images.map(image => {
+                return(
+                    <div>
+                        <img src={image} width="150"/>
+                    </div>
+                );
+            })
+
+   }
+
+   
   return (
-    <img src="https://images.unsplash.com/photo-1643685623589-625d36ffb709?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" />
+    <section>
+        <div className="flex justify-center">    
+              <ShowImage />     
+                       
+        </div>
+
+    </section>
   );
 }
-
-
-/* export default class images extends PureComponent {
-
-    constructor(props){
-        console.log(props);
-        super(props);
-        this.state = { interval: null};
-    }
-    componentDidMount(){
-        console.log("Images component mounted");
-        this.setState({
-           interval: setInterval( () => {
-            console.log("Hello");
-        }, 1000
-
-        )
-        })
-        
-    }
-
-    componentWillUnmount(){
-        console.log ("Images component Unmounted");
-        clearInterval(this.state.interval);
-       
-     }
-
-  render() {
-    return ( 
-
-    <img src="https://images.unsplash.com/photo-1643685623589-625d36ffb709?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" />
-    
-    );
-  }
-}
- */
