e9c498469679d6f4e91ca3bdc5760784303bc63c
nnk-maker <krishnanarasimha4@gmail.com>
2022-02-27 16:43:14 +0100

    error handling with out unnecessary code
    error handling with out unnecessary code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,64 +26,6 @@ import Images from "./components/Images";
 }  
 
  
-//  class App extends React.Component{
-//     /* constructor(props){
-//        // console.log("App Constructor");
-//         super(props);
-//         this.state={title: "Hello React 2", isShowing:false};
-//       //  this.handleClick = this.handleClick.bind(this);
 
-//     } */
-
-//     //Arrow function
-//     /* handleClick = () => {
-//         this.setState({isShowing: !this.state.isShowing})
-//      } */
-
-//      componentDidMount() {
-//          console.log("App Mounted");
-//         // this.setState({title: "HelloLifeCycle"});
-//      }
-
-//      componentDidUpdate(){
-//          console.log("App Updated");
-//      }
-     
-//     /* handleClick = () =>{
-//         this.setState({isShowing: !this.state.isShowing})
-//      } */
-//     // sates are immutable
-//     render(){
-//         console.log("App render");
-//         return(
-//             <section className="flex justify-center">
-//                     <div className="w-1/2">
-//                       <div className="text-center">
-//                         <div className="my-4">{this.state.title}</div>
-                    
-//                         <div>
-//                            <button className="p-1 bg-blue-700 text-white my-2"
-//                             onClick={this.handleClick}
-//                             >Toggle Image
-//                             </button>
-                           
-//                         </div>
-//                         {
-//                             this.state.isShowing ? 
-//                             ( <Images/> ) 
-//                             : null
-                           
-
-//                         }
-                        
-//                       </div>
-                     
-//                     </div>
-//             </section>
-             
-//              );
-//     }
-// }
- 
 
 export default App;
--- my-app/src/components/Images.js
+++ my-app/src/components/Images.js
@@ -7,28 +7,10 @@ import Image from './Image';
 
 export default function Images() {
     
-    //const [images, setimages] = useState([ ]);
+    
     const [page, setPage] = useState(1);
     const [images, setImages, errors] = useFetchImage(page);
-    // const scrollPosition = useScroll();
-   // const inputRef = useRef(null);
-
-
-
-   /*  useEffect(()=>{
-           inputRef.current.focus();
-            // console.log(process.env);
-            
-           // console.log(varRef);
     
-    },[]); */
-
-    
-
-    
-    //const [newImageUrl, setNewImageUrl] = useState("");
-    
-
    function handleRemove(index) {
            setImages([
            ...images.slice(0,index), 
@@ -50,23 +32,7 @@ export default function Images() {
 
    }
 
-  /*  function handleAdd() {
-       
-       if(newImageUrl != ""){
-            setImages([newImageUrl, ...images,]);
-            setNewImageUrl("");
-       }
-       
-       //images.push();
-       //console.log(tempImage);
-
-   }
- */
-   /* function handleChange(event) {
-       setNewImageUrl(event.target.value);
-       //console.log(event.target.value);
-   } */
-
+ 
    
   return (
     <section>
@@ -87,27 +53,9 @@ export default function Images() {
             errors.length === 0 && (
                 <button onClick={()=>{setPage(page + 1)}}>Load More</button>
                 )
-                
+
         }
         
-             {/*  <div className="flex justify-between my-5">
-                <div className="w-full">
-                    <input type="text" 
-                    id="inputBox"
-                    ref={inputRef}
-                    className="p-2 border border-gray-800 shadow rounded w-full"
-                    value={newImageUrl}
-                    onChange = {handleChange}
-                    />
-                </div>
-
-                    <div className="">
-                        <button 
-                            disabled={newImageUrl == ""}
-                            className= {`p-2 text-white ml-2 ${newImageUrl != "" ? "bg-green-600": "bg-green-300"}`} 
-                            onClick={handleAdd}>Add</button>
-                    </div>    
-              </div>    */}
                        
        
 
