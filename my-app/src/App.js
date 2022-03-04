9794b56c029ecf80646f918cab43ecf6424d5284
nnk-maker <krishnanarasimha4@gmail.com>
2022-03-04 11:57:21 +0100

    latest commit
    latest commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,13 @@
 import "./assets/css/style.css"
 import Images from "./components/Images";
 
+import {BrowserRouter, BrowserRouter as Route, Routes} from  "react-router-dom";
 
+function Gallery(){
 
+  return (
 
- function App() {
-    /* const [title, setTitle] = useState("Hellow React"); */
-          
-    return (
-        <section className="flex justify-center">
+    <section className="flex justify-center">
            
                     <div className="w-10/12">
                       <div className="text-center">
@@ -21,7 +20,20 @@ import Images from "./components/Images";
                       
                     </div>
             </section>
-    )
+  );
+
+
+}
+
+ function App() {
+    /* const [title, setTitle] = useState("Hellow React"); */
+          
+    return (<BrowserRouter>
+                <Routes>
+                    <Route path="/gallery"  element={ <Gallery/>}/>
+                </Routes>
+                  
+          </BrowserRouter> );
     
 }  
 
